49784e132a9b944c989958f22993928fea6654a7
mathpvx <7547@holbertonstudents.com>
2025-10-30 10:28:13 +0100

    test displayed components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { useState } from 'react'
+import Input from './components/general/Input'
+import SelectInput from './components/general/SelectInput'
+import Button from './components/general/Button'
+import SearchBar from './components/general/SearchBar'
+
 export default function App() {
-  return <div className="App"></div>;
+  const [username, setUsername] = useState('')
+  const [minDate, setMinDate] = useState('')
+  const [sort, setSort] = useState('Default')
+  const [title, setTitle] = useState('')
+
+  const options = ['Default', 'Latest', 'Oldest', 'Highest Rated', 'Lowest Rated']
+
+  return (
+    <div style={{ padding: '2rem', background: '#111', minHeight: '100vh', color: 'white' }}>
+      <h2>General Components Preview</h2>
+      <Input label="Username:" value={username} setValue={setUsername} />
+      <Input label="Min Date:" type="number" value={minDate} setValue={setMinDate} />
+      <SelectInput label="Sort:" options={options} value={sort} setValue={setSort} />
+      <Button label="Load More.." onClick={() => alert('Clicked!')} />
+      <SearchBar title={title} setTitle={setTitle} />
+    </div>
+  )
 }
